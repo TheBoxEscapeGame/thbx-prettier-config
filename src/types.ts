3a602eb535f74ef85cc20b8850335a01c7e93a7f
Philippe Muller <philippe.muller@gmail.com>
2021-10-06 13:57:11 +0200

    Import project

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+import { Options as BaseOptions } from "prettier";
+
+export type Options = BaseOptions & {
+  // @trivago/prettier-plugin-sort-imports
+  importOrder?: string[];
+  importOrderSeparation?: boolean;
+};
